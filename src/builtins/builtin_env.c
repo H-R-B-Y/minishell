6f794812e808a72ccc4a5af2a127490eaf0b5532
cquinter <cquinter@student.42london.com>
2025-07-02 18:00:37 +0100

    export and env tested with pipes. env now takes in multiple argvs, but it does not run programs. Pending to test exit handling and returncode handling

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -6,21 +6,38 @@
 /*   By: cquinter <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 13:34:33 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/06/29 11:46:51 by cquinter         ###   ########.fr       */
+/*   Updated: 2025/07/02 17:57:29 by cquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../include/builtin.h"
 
-void	*print_and_ret(void *p);
-
 int	builtin_env(t_minishell *shell, char **argv, char ***envp)
 {
-	(void)argv;
-	(void)envp;
-	(void)shell;
-	ft_arriter((void *)shell->environment, print_and_ret);
-	ft_arriter((void *)(*envp), print_and_ret);
+	char	**env;
+	char	*sep;
+	size_t	i;
+	
+	if (!argv[1] && !(*envp))
+		return (ft_arriter((void *)shell->environment, print_and_ret), 0);
+	env = (char **)ft_arrmap((void **)shell->environment,
+		(void *)ft_strdup, free);
+	if (!env)
+		return (perror("minishell: builtin_env"), 1);
+	if (set_n_envp(&env, *envp, ft_arrlen((void *)(*envp))) == -1)
+		return (ft_dirtyswap((void *)&env, NULL, free), 1);
+	i = 1;
+	while (argv[i])
+	{
+		sep = ft_strchr(argv[i], '=');
+		if (!sep)
+			return (ft_dirtyswap((void *)&env, NULL, free), 
+				ft_putstr_fd("minishell: env: Unable to run program\n", 2), 1);
+		if (set_n_envp(&env, argv + i, 1) == -1)
+			return (ft_dirtyswap((void *)&env, NULL, free), 1);
+		i++;
+	}
+	ft_arriter((void *)env, print_and_ret);
 	return (0);
 }
