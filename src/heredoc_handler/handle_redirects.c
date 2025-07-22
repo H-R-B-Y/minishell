97cc24c2aed6cb51bff9702b5d4f1350e48f86db
H-R-B-Y <harveyraybreeze@gmail.com>
2025-07-22 17:34:02 +0100

    ok some more robust handling in the ast constructor and redirect handlers

--- src/heredoc_handler/handle_redirects.c
+++ src/heredoc_handler/handle_redirects.c
@@ -6,7 +6,7 @@
 /*   By: hbreeze <hbreeze@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 16:49:12 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/06/25 16:21:03 by hbreeze          ###   ########.fr       */
+/*   Updated: 2025/07/22 17:22:55 by hbreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,20 @@ t_redirect_desc *handle_redirectfd(t_token *redirect)
 	char			*sep;
 
 	output = ft_calloc(1, sizeof(t_redirect_desc));
+	if (!output)
+		return (NULL);
 	sep = ft_strchr(redirect->raw, '>');
 	if (!sep)
 		sep = ft_strchr(redirect->raw, '<');
 	extract_tofromfds(redirect->raw, sep, fds);
 	if (redirect->raw[ft_strlen(redirect->raw) - 1] != '-')
 		(*output) = (t_redirect_desc){
-		.type = REDIRECT_FD,
-		.subtype = REDIR_FD,
-		.fd_map.from_fd = fds[1],
-		.fd_map.to_fd = fds[0],
-		};
+		.type = REDIRECT_FD, .fd_map.from_fd = fds[1],
+		.subtype = REDIR_FD, .fd_map.to_fd = fds[0],};
 	else
 		(*output) = (t_redirect_desc){
-		.type = REDIRECT_FD,
-		.subtype = CLOSE_FD,
-		.fd_map.from_fd = fds[1],
-		.fd_map.to_fd = fds[0],
-		};
+		.type = REDIRECT_FD, .fd_map.from_fd = fds[1],
+		.subtype = CLOSE_FD, .fd_map.to_fd = fds[0],};
 	return (output);
 }
 
@@ -85,14 +81,12 @@ t_redirect_desc *handle_redirect(t_token *redirect, t_token *filename)
 		to_fd = -1;
 	else
 		to_fd = get_tofd(redirect->raw, sep);
-	(*output) = (t_redirect_desc){
-		.type = (t_redirect_type [3])
-		{REDIRECT_INPUT, REDIRECT_OUTPUT, REDIRECT_OUTPUT_APPEND}[
+	(*output) = (t_redirect_desc){.type = (t_redirect_type [3]){
+		REDIRECT_INPUT, REDIRECT_OUTPUT, REDIRECT_OUTPUT_APPEND}[
 			(0 * (redirect->type == TOK_REDIR_IN))
 			+ (1 * (redirect->type == TOK_REDIR_OUT))
 			+ (2 * (redirect->type == TOK_REDIR_APPEND))],
-		.file_map.filename = ft_strdup(filename->raw),
-		.file_map.to_fd = to_fd,
+		.file_map.filename = ft_strdup(filename->raw), .file_map.to_fd = to_fd,
 	};
 	return (output);
 }
